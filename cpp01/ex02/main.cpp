4a612ab6e4775c8d0e149cae0e873a85b0737def
rikeda <rikeda@penguin>
2024-02-06 22:46:07 +0900

    [update] ex02 main.cpp

--- cpp01/ex02/main.cpp
+++ cpp01/ex02/main.cpp
@@ -5,13 +5,13 @@ int	main(void) {
 	std::string*	stringPTR = &string;
 	std::string&	stringREF = string;
 
-	std::cout << "string    memory = " << &string << std::endl;
-	std::cout << "stringPTR memory = " << stringPTR << std::endl;
-	std::cout << "stringREF memory = " << &stringREF << std::endl;
+	std::cout << "string         memory address = " << &string << std::endl;
+	std::cout << "stringPTR held memory address = " << stringPTR << std::endl;
+	std::cout << "stringREF held memory address = " << &stringREF << std::endl;
 
-	std::cout << "string           = \"" << string << "\"" << std::endl;
-	std::cout << "stringPTR        = " << *stringPTR << "\"" << std::endl;
-	std::cout << "stringREF        = " << stringREF << "\"" << std::endl;
+	std::cout << "string               value    = \"" << string << "\"" << std::endl;
+	std::cout << "stringPTR pointed to value    = \"" << *stringPTR << "\"" << std::endl;
+	std::cout << "stringREF pointed to value    = \"" << stringREF << "\"" << std::endl;
 
 	return 0;
 }
