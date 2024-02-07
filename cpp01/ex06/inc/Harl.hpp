63038c9b08e36e22b39fc3e793406c2cca5d364f
rikeda-cloud <longsichitian648@gmail.com>
2024-02-07 19:00:21 +0900

    [update] ex05と同じような実装に変更した

--- cpp01/ex06/inc/Harl.hpp
+++ cpp01/ex06/inc/Harl.hpp
@@ -1,45 +1,26 @@
-#ifndef CPP01_EX05_H_
-# define CPP01_EX05_H_
+#ifndef CPP01_EX06_Harl_H_
+# define CPP01_EX06_Harl_H_
 
 #include <string>
 
-
 class Harl {
-public:
-	typedef void	(*harl_func_ptr)();
 private:
-	ssize_t	_level_to_index(std::string);
+	void	(Harl::*f_list_[4])(void);
+	std::string	level_list_[4];
+
+	void	debug(void);
+	void	info(void);
+	void	warning(void);
+	void	error(void);
+	ssize_t	LevelToIndex(std::string&);
+
 public:
 	Harl(void);
-	static void	debug(void);
-	static void	info(void);
-	static void	warning(void);
-	static void	error(void);
+	Harl(const Harl&);
+	~Harl(void);
+	Harl&	operator=(const Harl&);
+	
 	void	complain(std::string level);
-};
-
-class HarlPair {
-private:
-	std::string			key_;
-	Harl::harl_func_ptr	value_;
-public:
-	HarlPair(void);
-	HarlPair(std::string, Harl::harl_func_ptr);
-	void					set(std::string, Harl::harl_func_ptr);
-	const std::string		getKey(void);
-	Harl::harl_func_ptr		getValue(void);
-};
-
-class HarlDict {
-private:
-	int			size_;
-	HarlPair	*dict_;
-public:
-	HarlDict(void);
-	~HarlDict(void);
-	void		append(std::string, Harl::harl_func_ptr);
-	int			size(void);
-	HarlPair	*getHarlPair(int idx);
 };
 
 #endif
