d172cab340ec9a866ca68c7309ed9064aeb3e9ca
rikeda-cloud <longsichitian648@gmail.com>
2024-02-11 15:55:00 +0900

    [update] ex00の完成、出力メッセージは改善する必要があるかも？

--- cpp03/ex00/srcs/main.cpp
+++ cpp03/ex00/srcs/main.cpp
@@ -5,20 +5,22 @@ int	main(void) {
 	ClapTrap	enemy("enemy");
 
 	my.attack(enemy.getName());
-	my.takeDamege(1);
+	my.takeDamage(1);
 	my.beRepaired(1);
-	my.takeDamege(9);
-	my.takeDamege(1);
+	my.attack(enemy.getName());
+	my.takeDamage(8);
+	my.beRepaired(1);
+	my.takeDamage(1);
+	my.takeDamage(1);
+	my.beRepaired(1);
 	my.beRepaired(1);
 	my.beRepaired(1);
 	my.beRepaired(1);
 	my.attack(enemy.getName());
-	my.attack(enemy.getName());
-	my.attack(enemy.getName());
-	my.attack(enemy.getName());
-	my.attack(enemy.getName());
 	my.beRepaired(1);
 	my.beRepaired(1);
+	my.takeDamage(100);
 	my.beRepaired(1);
+
 	return 0;
 }
