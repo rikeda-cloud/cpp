82884978c5143b13992def0cf43dc2400d16bf5f
rikeda-cloud <longsichitian648@gmail.com>
2024-02-12 20:34:01 +0900

    [add] ex00の一旦の完成

---
+++ cpp05/ex00/srcs/Bureaucrat.cpp
@@ -0,0 +1,52 @@
+#include "Bureaucrat.hpp"
+#include <iostream>
+
+Bureaucrat::Bureaucrat(std::string name, size_t grade) : name_(name) {
+	if (grade < GRADE_HIGH_LIMIT)
+		throw Bureaucrat::GradeTooHighException();
+	if (GRADE_LOW_LIMIT < grade)
+		throw Bureaucrat::GradeTooLowException();
+	grade_ = grade;
+	std::cout << "Bureaucrat constructor called" << std::endl;
+}
+
+Bureaucrat::Bureaucrat(const Bureaucrat& bureaucrat) {
+	*this = bureaucrat;
+	std::cout << "Bureaucrat copy constructor called" << std::endl;
+}
+
+Bureaucrat::~Bureaucrat(void) {
+	std::cout << "Bureaucrat destructor called" << std::endl;
+}
+
+Bureaucrat&	Bureaucrat::operator=(const Bureaucrat& bureaucrat) {
+	if (this != &bureaucrat) {
+		this->name_ = bureaucrat.name_;
+		this->grade_ = bureaucrat.grade_;
+	}
+	return *this;
+}
+
+const std::string&	Bureaucrat::getName(void) const {
+	return name_;
+}
+
+size_t	Bureaucrat::getGrade(void) const {
+	return grade_;
+}
+
+void	Bureaucrat::GradeUp(size_t grade_increase) {
+	if (GRADE_LOW_LIMIT <= grade_increase)
+		throw Bureaucrat::GradeTooHighException();
+	if (grade_ <= grade_increase)
+		throw Bureaucrat::GradeTooHighException();
+	grade_ -= grade_increase;
+}
+
+void	Bureaucrat::GradeDown(size_t grade_decrease) {
+	if (GRADE_LOW_LIMIT <= grade_decrease)
+		throw Bureaucrat::GradeTooLowException();
+	if (GRADE_LOW_LIMIT <= grade_decrease + grade_)
+		throw Bureaucrat::GradeTooLowException();
+	grade_ += grade_decrease;
+}
---
+++ cpp05/ex00/srcs/main.cpp
@@ -0,0 +1,39 @@
+#include "Bureaucrat.hpp"
+#include <iostream>
+
+int	main(void) {
+	try {
+		Bureaucrat	b("my", 0);
+	}
+	catch (std::exception& e) {
+		std::cout << "Error!!" << std::endl;
+	}
+	try {
+		Bureaucrat	b("my", 151);
+	}
+	catch (std::exception& e) {
+		std::cout << "Error!!" << std::endl;
+	}
+	try {
+		Bureaucrat	b("my", 150);
+		while (true) {
+			b.GradeUp(3);
+			std::cout << b.getGrade() << std::endl;
+		}
+	}
+	catch (std::exception& e) {
+		std::cout << "Error!!" << std::endl;
+	}
+	try {
+		Bureaucrat	b("my", 1);
+		while (true) {
+			b.GradeDown(3);
+			std::cout << b.getGrade() << std::endl;
+		}
+	}
+	catch (std::exception& e) {
+		std::cout << "Error!!" << std::endl;
+	}
+
+	return 0;
+}
