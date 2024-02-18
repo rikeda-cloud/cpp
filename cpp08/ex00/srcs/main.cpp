a845291630732d4f4cb59d107afba608f378d77e
rikeda-cloud <longsichitian648@gmail.com>
2024-02-18 19:14:18 +0900

    [add] ex00OK

--- cpp08/ex00/srcs/main.cpp
+++ cpp08/ex00/srcs/main.cpp
@@ -1,29 +1,15 @@
+#include "easyfind.hpp"
 #include <vector>
 #include <list>
 #include <iostream>
-
-// template<class Container>
-// void	set(const Container& container) {
-// 	int i = 0;
-// 	for (const class Container::value_type& element : container) {
-// 		element = i++;
-// 	}
-// 	std::cout << std::endl;
-// }
-
-template<typename T>
-void	find(const T& container, int value) {
-	for (typename T::const_iterator it = container.begin(); it != container.end(); ++it) {
-		std::cout << *it << std::endl;
-	}
-	(void)value;
-}
 
 int	main(void) {
 	std::vector<int>	v1(5);
 	std::list<int>		v2(5);
 
-	  find(v1, 1);
-	  find(v2, 1);
+	for (size_t i = 0; i < 5; i++)
+		v1[i] = 5 - i;
+
+	std::cout << easyfind(v1, 1) << std::endl;
 	return 0;
 }
