07b14ad84dc348ed45737b9d3c15c49da1468020
rikeda-cloud <longsichitian648@gmail.com>
2024-02-19 17:34:48 +0900

    [WIP]

--- cpp08/ex02/inc/MutantStack.hpp
+++ cpp08/ex02/inc/MutantStack.hpp
@@ -4,40 +4,59 @@
 #include <stack>
 
 template<class T>
-class MutantStack {
+class MutantStack : public std::stack<T> {
 public:
 	MutantStack(void);
-	MutantStack(const MutantStack&);
+	MutantStack(const MutantStack& stack);
 	~MutantStack(void);
-	MutantStack&	operator=(const MutantStack&);
+	MutantStack&	operator=(const MutantStack& stack);
 
-	T&		top(void) const;
-	bool	empty(void) const;
-	size_t	size(void) const;
-	void	push(T);
-	T		pop(void);
-	MutantStack<T>*	begin(void);
-	MutantStack<T>*	end(void);
-
-	bool	operator==(const MutantStack&) const;
-	bool	operator!=(const MutantStack&) const;
-	bool	operator<(const MutantStack&) const;
-	bool	operator<=(const MutantStack&) const;
-	bool	operator>(const MutantStack&) const;
-	bool	operator>=(const MutantStack&) const;
-
-	template<T>
+	// template<T>
 	class iterator {
 	public:
-		iterator(T* ptr) : ptr_(ptr) {};
+		T*	ptr_;
+
+		iterator(const iterator&);
+		~iterator(void);
+		iterator&	operator=(const iterator&);
+
+		T&			operator*(void);
+		iterator&	operator++(void);
+		iterator	operator++(int);
+		iterator&	operator--(void);
+		iterator	operator--(int);
+		bool		operator!=(const iterator&);
 
 	private:
-		T*	ptr_;
+		iterator(void);
 	};
 
-
-private:
-	std::stack<T>	c;
+	iterator	begin(void);
+	iterator	end(void);
 };
+
+#include <iostream>
+
+template<class T>
+MutantStack<T>::MutantStack(void) {}
+
+template<class T>
+MutantStack<T>::MutantStack(const MutantStack<T>& stack) : std::stack<T>(stack){
+}
+
+template<class T>
+MutantStack<T>::~MutantStack(void) {
+	std::cout << "delete MutantStack" << std::endl;
+}
+
+template<class T>
+MutantStack<T>&	MutantStack<T>::operator=(const MutantStack<T>& stack) {
+	if (this != &stack) {
+		*this = stack;
+	}
+	return *this;
+}
+
+// #include "MutantStack.tpp"
 
 #endif
