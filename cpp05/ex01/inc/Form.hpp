6ff10f42f44dd0c2be8f61f5da926308155564b7
rikeda-cloud <longsichitian648@gmail.com>
2025-04-27 17:37:24 +0900

    [add] Form.hpp, Bureaucrat.signForm

---
+++ cpp05/ex01/inc/Form.hpp
@@ -0,0 +1,42 @@
+#ifndef CPP05_EX01_Form_H_
+#define CPP05_EX01_Form_H_
+
+#include "Bureaucrat.hpp"
+#include <ostream>
+#include <string>
+
+class Form {
+public:
+  static const size_t GRADE_HIGH_LIMIT = 1;
+  static const size_t GRADE_LOW_LIMIT = 150;
+
+  class GradeTooHighException : public std::exception {
+  public:
+    const char *what() const throw();
+  };
+  class GradeTooLowException : public std::exception {
+  public:
+    const char *what() const throw();
+  };
+
+  Form(std::string, size_t, size_t);
+  Form(const Form &);
+  ~Form(void);
+  Form &operator=(const Form &);
+
+  const std::string &getName(void) const;
+  bool is_signed(void) const;
+  const size_t getGradeToSign(void) const;
+  const size_t getGradeToExecute(void) const;
+  void beSigned(const Bureaucrat &);
+
+private:
+  const std::string name_;
+  bool is_signed_;
+  const size_t grade_to_sign_;
+  const size_t grade_to_execute_;
+};
+
+std::ostream &operator<<(std::ostream &s, const Form &form);
+
+#endif
