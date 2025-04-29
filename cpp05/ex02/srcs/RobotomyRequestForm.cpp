3d6ffd69e2192c187c039f54bf1bd9069fc224ff
rikeda-cloud <longsichitian648@gmail.com>
2025-04-29 16:03:12 +0900

    [update] RobotomyRequestForm.cpp

---
+++ cpp05/ex02/srcs/RobotomyRequestForm.cpp
@@ -0,0 +1,38 @@
+#include "RobotomyRequestForm.hpp"
+#include "AForm.hpp"
+#include <ctime>
+#include <iostream>
+
+RobotomyRequestForm::RobotomyRequestForm(std::string target)
+    : AForm("RobotomyRequestForm", RobotomyRequestForm::REQUIRED_GRADE_SIGN,
+            RobotomyRequestForm::REQUIRED_GRADE_EXEC),
+      target_(target) {}
+
+RobotomyRequestForm::RobotomyRequestForm(const RobotomyRequestForm &form)
+    : AForm(form), target_(form.target_) {}
+
+RobotomyRequestForm::~RobotomyRequestForm(void) {}
+
+RobotomyRequestForm &
+RobotomyRequestForm::operator=(const RobotomyRequestForm &form) {
+  if (this != &form) {
+    AForm::operator=(form);
+    const_cast<std::string &>(this->target_) = form.target_;
+  }
+  return *this;
+}
+
+void RobotomyRequestForm::action(void) const {
+  static bool seeded = false;
+  if (!seeded) {
+    std::srand(std::time(0));
+    seeded = true;
+  }
+
+  if (std::rand() % 2 == 0) {
+    std::cout << "[SUCCESS] " << target_ << "has become a robot" << std::endl;
+  } else {
+    std::cout << "[ERROR] " << target_ << "couldn't become a robot"
+              << std::endl;
+  }
+}
