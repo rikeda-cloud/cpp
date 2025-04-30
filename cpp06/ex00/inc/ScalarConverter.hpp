e0f48b86864906c45b23c60eb97b826bf0fe24bf
rikeda-cloud <longsichitian648@gmail.com>
2025-04-30 18:55:32 +0900

    [WIP]

--- cpp06/ex00/inc/ScalarConverter.hpp
+++ cpp06/ex00/inc/ScalarConverter.hpp
@@ -1,11 +1,18 @@
 #ifndef CPP06_EX00_ScalarConverter_H_
 #define CPP06_EX00_ScalarConverter_H_
 
+#include "ScalarType.hpp"
 #include <string>
 
 class ScalarConverter {
 public:
   static void convert(const std::string &);
+  static ScalarType detectScalarType(const std::string &);
+
+  static bool isCharType(const std::string &);
+  static bool isIntType(const std::string &);
+  static bool isFloatType(const std::string &);
+  static bool isDoubleType(const std::string &);
 
 private:
   ScalarConverter(void);
---
+++ cpp06/ex00/inc/ScalarType.hpp
@@ -0,0 +1,12 @@
+#ifndef CPP06_EX00_ScalarType_H_
+#define CPP06_EX00_ScalarType_H_
+
+enum ScalarType {
+  Char,
+  Int,
+  Float,
+  Double,
+  Unknown,
+};
+
+#endif
