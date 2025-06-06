794022763ed809635e414046ee08864bc3d670f5
rikeda-cloud <longsichitian648@gmail.com>
2025-06-06 19:03:32 +0900

    [add] FloatConverter::convertTo

--- cpp06/ex00/srcs/FloatConverter.cpp
+++ cpp06/ex00/srcs/FloatConverter.cpp
@@ -1,6 +1,7 @@
 #include "FloatConverter.hpp"
+#include "FloatScalar.hpp"
 #include "IScalar.hpp"
-#include <iostream>
+#include <cstdlib>
 
 FloatConverter::FloatConverter(void) : AConverter() {}
 
@@ -9,6 +10,12 @@ FloatConverter::FloatConverter(AConverter *converter) : AConverter(converter) {}
 FloatConverter::~FloatConverter(void) {}
 
 IScalar *FloatConverter::convertTo(const std::string &s) const {
-  std::cout << "call convertToFloat " << s << std::endl;
+  char *end;
+  float value = std::strtof(s.c_str(), &end);
+
+  if (s.length() > 1 && std::string(end) == "f") {
+    return new FloatScalar(value);
+  }
+
   return NULL;
 }
