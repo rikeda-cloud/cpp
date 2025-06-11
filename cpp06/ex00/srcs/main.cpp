36bb643e4387833d0f4f2f8c35fe95f531a35421
rikeda-cloud <longsichitian648@gmail.com>
2025-06-11 17:46:49 +0900

    [update] main.cpp

--- cpp06/ex00/srcs/main.cpp
+++ cpp06/ex00/srcs/main.cpp
@@ -1,25 +1,9 @@
-#include "AConverter.hpp"
-#include "CharConverter.hpp"
-#include "DoubleConverter.hpp"
-#include "FloatConverter.hpp"
-#include "IScalar.hpp"
-#include "IntConverter.hpp"
-#include <iostream>
+#include "ScalarConverter.hpp"
 
-int main(void) {
-  IntConverter cvt;
-  CharConverter cvt2(&cvt);
-  FloatConverter cvt3(&cvt2);
-  DoubleConverter cvt4(&cvt3);
-
-  IScalar *scalar = cvt4.convert("42");
-  if (scalar) {
-    std::cout << "Int: " << scalar->castToInt() << std::endl;
-    std::cout << "Char: " << scalar->castToChar() << std::endl;
-    std::cout << "Float: " << scalar->castToFloat() << std::endl;
-    std::cout << "Double: " << scalar->castToDouble() << std::endl;
-  } else {
-    std::cout << "Error" << std::endl;
+int main(int argc, char **argv) {
+  if (argc != 2) {
+    return 1;
   }
+  ScalarConverter::convert(argv[1]);
   return 0;
 }
