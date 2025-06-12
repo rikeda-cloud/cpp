761e5dfdc39e5f0680671c3a3c32dbb53231acb0
rikeda-cloud <longsichitian648@gmail.com>
2025-06-12 17:07:16 +0900

    [update] アサート失敗時の出力メッセージを増やした

--- cpp06/ex00/tests/test_utils.cpp
+++ cpp06/ex00/tests/test_utils.cpp
@@ -10,19 +10,16 @@
 #define GREEN "\033[32m"
 #define RESET "\033[0m"
 
-bool _assert_string(const std::string &expect, const std::string &actual,
-                    const std::string &type) {
+bool _assert_string(const std::string &param, const std::string &expect,
+                    const std::string &actual, const std::string &type) {
   if (expect == actual) {
-    std::cout << GREEN << "[OK] " << RESET << std::endl;
     return 0;
-  } else {
-    std::cout << RED << "[FAIL"
-              << "(" << type << ")"
-              << "] "
-              << "Expected: \"" << expect << "\", but got: \"" << actual << "\""
-              << RESET << std::endl;
-    return 1;
   }
+
+  std::cout << RED << "[FAIL] " << param << "(" << type << ")" << std::endl
+            << "  Expected: \"" << expect << "\"" << std::endl
+            << "  Actual  : \"" << actual << "\"" << RESET << std::endl;
+  return 1;
 }
 
 bool exec_test_convert(const std::string &param, const std::string &expect_char,
@@ -36,11 +33,15 @@ bool exec_test_convert(const std::string &param, const std::string &expect_char,
 
   IScalar *scalar = cvt4.convert(param);
 
-  int fail_count = 0;
-  fail_count += _assert_string(expect_char, scalar->castToChar(), "Char");
-  fail_count += _assert_string(expect_int, scalar->castToInt(), "Int");
-  fail_count += _assert_string(expect_float, scalar->castToFloat(), "Float");
-  fail_count += _assert_string(expect_double, scalar->castToDouble(), "Double");
+  int fail_count =
+      _assert_string(param, expect_char, scalar->castToChar(), "Char") +
+      _assert_string(param, expect_int, scalar->castToInt(), "Int") +
+      _assert_string(param, expect_float, scalar->castToFloat(), "Float") +
+      _assert_string(param, expect_double, scalar->castToDouble(), "Double");
+
+  if (!fail_count) {
+    std::cout << GREEN << "[OK] " << param << RESET << std::endl;
+  }
 
   return fail_count >= 1;
 }
