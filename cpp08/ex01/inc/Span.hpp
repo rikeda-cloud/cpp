c9d910aa2119aa88c3f469d3a9d4e96c895c4769
rikeda-cloud <longsichitian648@gmail.com>
2025-06-15 15:26:58 +0900

    [add] ex01大雑把な実装

---
+++ cpp08/ex01/inc/Span.hpp
@@ -0,0 +1,25 @@
+#ifndef CPP08_EX01_Span_H_
+#define CPP08_EX01_Span_H_
+
+class Span {
+public:
+  Span(unsigned int);
+  ~Span(void);
+  Span(const Span &);
+  Span &operator=(const Span &);
+
+  void addNumber(int);
+  unsigned int shortestSpan(void) const;
+  unsigned int longestSpan(void) const;
+
+private:
+  unsigned int n_;
+  unsigned int crr_idx_;
+  unsigned int shortest_span_;
+  unsigned int longest_span_;
+  int *array_;
+
+  Span(void);
+};
+
+#endif
---
+++ cpp08/ex01/srcs/Span.cpp
@@ -0,0 +1,71 @@
+#include "Span.hpp"
+#include <cmath>
+#include <limits>
+#include <stdexcept>
+
+Span::Span(unsigned int n)
+    : n_(n), crr_idx_(0),
+      shortest_span_(std::numeric_limits<unsigned int>::max()),
+      longest_span_(0), array_(new int(n)) {}
+
+Span::~Span(void) { delete this->array_; }
+
+Span::Span(const Span &span)
+    : n_(span.n_), crr_idx_(span.crr_idx_), shortest_span_(span.shortest_span_),
+      longest_span_(span.longest_span_), array_(new int(span.n_)) {
+  for (unsigned int i = 0; i < this->crr_idx_; ++i) {
+    this->array_[i] = span.array_[i];
+  }
+}
+
+Span &Span::operator=(const Span &span) {
+  if (this != &span) {
+    this->n_ = span.n_;
+    this->crr_idx_ = span.crr_idx_;
+    this->shortest_span_ = span.shortest_span_;
+    this->longest_span_ = span.longest_span_;
+    delete this->array_;
+    this->array_ = new int(this->n_);
+    for (unsigned int i = 0; i < this->crr_idx_; ++i) {
+      this->array_[i] = span.array_[i];
+    }
+  }
+
+  return *this;
+}
+
+void Span::addNumber(int number) {
+  if (this->crr_idx_ >= this->n_) {
+    throw std::length_error("FixedArray: exceeded maximum size");
+  }
+
+  this->array_[this->crr_idx_] = number;
+  this->crr_idx_++;
+
+  if (this->crr_idx_ >= 2) {
+    unsigned int diff = std::abs(this->array_[this->crr_idx_ - 2] -
+                                 this->array_[this->crr_idx_ - 1]);
+    if (diff < this->shortest_span_) {
+      this->shortest_span_ = diff;
+    }
+    if (diff > this->longest_span_) {
+      this->longest_span_ = diff;
+    }
+  }
+}
+
+unsigned int Span::shortestSpan(void) const {
+  if (this->crr_idx_ <= 1) {
+    throw std::domain_error(
+        "At least two elements are required to compute a difference.");
+  }
+  return this->shortest_span_;
+}
+
+unsigned int Span::longestSpan(void) const {
+  if (this->crr_idx_ <= 1) {
+    throw std::domain_error(
+        "At least two elements are required to compute a difference.");
+  }
+  return this->longest_span_;
+}
