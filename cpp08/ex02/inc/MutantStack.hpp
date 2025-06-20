1c95dab505b9b1d7f9111e614444c7a76a92825f
rikeda-cloud <longsichitian648@gmail.com>
2025-06-20 15:25:55 +0900

    [add] 過去に作成したEX08をコピー

---
+++ cpp08/ex02/inc/MutantStack.hpp
@@ -0,0 +1,40 @@
+#ifndef CPP08_EX02_MutantStack_H_
+#define CPP08_EX02_MutantStack_H_
+
+#include <deque>
+#include <iterator>
+#include <stack>
+
+template <class T, class Container = std::deque<T> >
+class MutantStack : public std::stack<T, Container> {
+public:
+  MutantStack(void);
+  MutantStack(const MutantStack &stack);
+  ~MutantStack(void);
+  MutantStack &operator=(const MutantStack &stack);
+
+  class iterator : public std::iterator<std::bidirectional_iterator_tag, T *> {
+  public:
+    iterator(T *ptr);
+    iterator(const iterator &);
+    ~iterator(void);
+    iterator &operator=(const iterator &);
+
+    T &operator*(void);
+    iterator &operator++(void);
+    iterator &operator--(void);
+    bool operator!=(const iterator &) const;
+
+  private:
+    T *ptr_;
+
+    iterator(void);
+  };
+
+  iterator begin(void);
+  iterator end(void);
+};
+
+#include "MutantStack.tpp"
+
+#endif
