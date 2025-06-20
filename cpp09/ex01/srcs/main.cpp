ba29bf77488fb313ca2a3e89ddc93a4868e489a4
rikeda-cloud <longsichitian648@gmail.com>
2025-06-20 17:55:16 +0900

    [update] Error message

--- cpp09/ex01/srcs/main.cpp
+++ cpp09/ex01/srcs/main.cpp
@@ -10,7 +10,7 @@ int main(int argc, char **argv) {
   try {
     std::cout << RPN::evaluate(argv[1]) << std::endl;
   } catch (const std::exception &e) {
-    std::cerr << e.what() << std::endl;
+    std::cerr << "Error" << std::endl;
     return 1;
   }
 
