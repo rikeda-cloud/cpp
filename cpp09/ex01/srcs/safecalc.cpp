2d0dcf85cf58f45cb743b31e5eaf57ce335c43e9
rikeda-cloud <longsichitian648@gmail.com>
2025-07-02 13:27:29 +0900

    [fix] 掛け算時のオーバーフロー・アンダーフローをケア

--- cpp09/ex01/srcs/safecalc.cpp
+++ cpp09/ex01/srcs/safecalc.cpp
@@ -1,6 +1,7 @@
 #include "safecalc.hpp"
 #include <exception>
 #include <limits>
+#include <locale>
 
 int safe_add(int v1, int v2) {
   if (v2 > 0 && v1 > std::numeric_limits<int>::max() - v2) {
@@ -30,4 +31,16 @@ int safe_div(int v1, int v2) {
   return v1 / v2;
 }
 
-int safe_mul(int v1, int v2) { return v1 * v2; }
+int safe_mul(int v1, int v2) {
+  long long llv1 = v1;
+  long long llv2 = v2;
+  long long result = llv1 * llv2;
+
+  if (result > std::numeric_limits<int>::max() ||
+      result < std::numeric_limits<int>::min()) {
+    // Overflow or underflow
+    throw std::exception();
+  }
+
+  return v1 * v2;
+}
