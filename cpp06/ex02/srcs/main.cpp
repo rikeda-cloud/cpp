7b3ffd042fca788de12359d342b96f859b0022bc
rikeda-cloud <longsichitian648@gmail.com>
2025-07-31 16:21:25 +0900

    [update] main.cppに例外処理を追加

--- cpp06/ex02/srcs/main.cpp
+++ cpp06/ex02/srcs/main.cpp
@@ -1,18 +1,24 @@
 #include "Base.hpp"
 #include "utils.hpp"
 #include <cstddef>
+#include <exception>
+#include <iostream>
 
 int main(void) {
-  Base *ptr = generate();
-  Base &ref = *ptr;
-  Base *nullp = NULL;
-  Base &nullr = *nullp;
+  try {
+    Base *ptr = generate();
+    Base &ref = *ptr;
 
-  identify(ptr);
-  identify(ref);
-  identify(nullp);
-  identify(nullr);
+    identify(ptr);
+    identify(ref);
+    delete ptr;
 
-  delete ptr;
+    Base *nullp = NULL;
+    Base &nullr = *nullp;
+    identify(nullp);
+    identify(nullr);
+  } catch (const std::exception &e) {
+    std::cout << e.what() << std::endl;
+  }
   return 0;
 }
