49279833eb00b1aeb5bae7e899c078f3cd911bcf
rikeda-cloud <longsichitian648@gmail.com>
2025-08-01 00:30:37 +0900

    [fix] add delete, try/catch

--- cpp06/ex00/srcs/ScalarConverter.cpp
+++ cpp06/ex00/srcs/ScalarConverter.cpp
@@ -5,6 +5,7 @@
 #include "FloatConverter.hpp"
 #include "IScalar.hpp"
 #include "IntConverter.hpp"
+#include <exception>
 #include <iostream>
 
 void ScalarConverter::convert(const std::string &s) {
@@ -13,9 +14,14 @@ void ScalarConverter::convert(const std::string &s) {
   FloatConverter cvt3(&cvt2);
   IntConverter cvt4(&cvt3);
 
-  IScalar *scalar = cvt4.convert(s);
-  std::cout << "char: " << scalar->castToChar() << std::endl;
-  std::cout << "int: " << scalar->castToInt() << std::endl;
-  std::cout << "float: " << scalar->castToFloat() << std::endl;
-  std::cout << "double: " << scalar->castToDouble() << std::endl;
+  try {
+    IScalar *scalar = cvt4.convert(s);
+    std::cout << "char: " << scalar->castToChar() << std::endl;
+    std::cout << "int: " << scalar->castToInt() << std::endl;
+    std::cout << "float: " << scalar->castToFloat() << std::endl;
+    std::cout << "double: " << scalar->castToDouble() << std::endl;
+    delete scalar;
+  } catch (const std::exception &e) {
+    std::cout << e.what() << std::endl;
+  }
 }
