81123680632e0ba5854f0e5403d8a190df87c485
rikeda-cloud <longsichitian648@gmail.com>
2025-08-02 18:43:19 +0900

    [update] min, max関数で比較結果が同じ場合に第二引数の値を返すかのテストを追加

--- cpp07/ex00/srcs/main.cpp
+++ cpp07/ex00/srcs/main.cpp
@@ -1,7 +1,8 @@
+#include "Data.hpp"
 #include "whatever.hpp"
 #include <iostream>
 
-int main(void) {
+void sample_main(void) {
   int a = 2;
   int b = 3;
   ::swap(a, b);
@@ -14,5 +15,37 @@ int main(void) {
   std::cout << "c = " << c << ", d = " << d << std::endl;
   std::cout << "min( c, d ) = " << ::min(c, d) << std::endl;
   std::cout << "max( c, d ) = " << ::max(c, d) << std::endl;
+}
+
+void test_return_second_value(void) {
+  // min, max関数で値が等しい場合に第二引数の値が返されるか？
+
+  // INFO 比較される値は同じ
+  int comparable_v = 1;
+  int incomparable_v1 = 1;
+  int incomparable_v2 = 2;
+  Data d1(comparable_v, incomparable_v1);
+  Data d2(comparable_v, incomparable_v2);
+
+  if (::min(d1, d2).getIncomparableV() == incomparable_v2 &&
+      ::max(d1, d2).getIncomparableV() == incomparable_v2) {
+    std::cout << "[OK]" << std::endl;
+  } else {
+    std::cout << "[ERROR]" << std::endl;
+  }
+
+  ::swap(d1, d2);
+  if (::min(d1, d2).getIncomparableV() == incomparable_v1 &&
+      ::max(d1, d2).getIncomparableV() == incomparable_v1) {
+    std::cout << "[OK]" << std::endl;
+  } else {
+    std::cout << "[ERROR]" << std::endl;
+  }
+}
+
+int main(void) {
+  sample_main();
+  test_return_second_value();
+
   return 0;
 }
