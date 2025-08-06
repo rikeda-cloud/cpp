7f63976e69f40d5099d24851d0e6bdeb2b44d80e
rikeda-cloud <longsichitian648@gmail.com>
2025-08-06 17:24:33 +0900

    [refactor] add comment

--- cpp08/ex02/inc/MutantStack.hpp
+++ cpp08/ex02/inc/MutantStack.hpp
@@ -4,16 +4,21 @@
 #include <deque>
 #include <stack>
 
+// INFO 内部で使用するコンテナを指定しない場合、dequeを使用
 template <typename T, typename Container = std::deque<T> >
 class MutantStack : public std::stack<T, Container> {
 public:
   MutantStack(void);
-  MutantStack(const MutantStack &other);
+  MutantStack(const MutantStack &);
   ~MutantStack(void);
-  MutantStack &operator=(const MutantStack &other);
+  MutantStack &operator=(const MutantStack &);
 
+  // 毎回書くと長いのでtypedef
   typedef typename Container::iterator container_iterator;
 
+  // INFO bidirectional_iterator(双方向イテレータ) Interface的な役割
+  // INFO [Iterator] https://cpprefjp.github.io/reference/iterator.html
+  // INFO [Iterator Tag] https://cpprefjp.github.io/reference/iterator/iterator_tag.html
   class iterator : public std::iterator<std::bidirectional_iterator_tag, T> {
   private:
     container_iterator it_;
