2c68a004c3f868d55745b34001910bc644eb9378
rikeda-cloud <longsichitian648@gmail.com>
2025-08-08 18:55:13 +0900

    [add] Test DataBase

---
+++ cpp09/ex00/inc/test.hpp
@@ -0,0 +1,6 @@
+#ifndef CPP09_EX00_test_H_
+#define CPP09_EX00_test_H_
+
+int test_data_base(void);
+
+#endif
---
+++ cpp09/ex00/tests/main_test.cpp
@@ -0,0 +1,6 @@
+#include "test.hpp"
+
+int main(void) {
+  int fail_count = test_data_base();
+  return fail_count;
+}
