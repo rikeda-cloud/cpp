3ee2a79c305a763397d37196dcc2b3e967db751d
rikeda-cloud <longsichitian648@gmail.com>
2025-08-14 18:02:36 +0900

    [add] utils::findInsertIdx

--- cpp09/ex02/inc/utils.hpp
+++ cpp09/ex02/inc/utils.hpp
@@ -1,6 +1,12 @@
 #ifndef CPP09_EX02_utils_H_
 #define CPP09_EX02_utils_H_
 
+#include "PairPointer.hpp"
+#include <cstddef>
+
 unsigned jacobsthal(unsigned);
+
+std::size_t findInsertIdx(const std::vector<PairPointer> &, const PairPointer &,
+                          std::size_t, std::size_t);
 
 #endif
--- cpp09/ex02/srcs/utils.cpp
+++ cpp09/ex02/srcs/utils.cpp
@@ -1,5 +1,27 @@
+#include "utils.hpp"
+
 unsigned jacobsthal(unsigned n) {
   if (n <= 1)
     return n;
   return jacobsthal(n - 1) + 2 * jacobsthal(n - 2);
+}
+
+std::size_t findInsertIdx(const std::vector<PairPointer> &sorted_pairs,
+                          const PairPointer &target_pair, std::size_t left_idx,
+                          std::size_t right_idx) {
+  while (left_idx <= right_idx) {
+    std::size_t middle_idx = (left_idx + right_idx) / 2;
+    PairPointer middle_pair = sorted_pairs[middle_idx];
+
+    if (target_pair < middle_pair) {
+      right_idx = middle_idx - 1;
+      continue;
+    }
+    if (target_pair > middle_pair) {
+      left_idx = middle_idx + 1;
+      continue;
+    }
+    return middle_idx;
+  }
+  return left_idx;
 }
