bf68c3e352a0ba2b87205bca7d260c4265421d2f
rikeda-cloud <longsichitian648@gmail.com>
2025-08-18 17:23:53 +0900

    [add] test_is_date test case

--- cpp09/ex00/tests/test_is_date.cpp
+++ cpp09/ex00/tests/test_is_date.cpp
@@ -11,13 +11,32 @@ static bool _exec_is_date(int y, int m, int d, bool success_validate) {
 }
 
 int test_is_date(void) {
-  int fail_count =
-      _exec_is_date(0, 1, 1, true) + _exec_is_date(2000, 2, 29, true) +
-      _exec_is_date(9999, 12, 31, true) + _exec_is_date(2001, 2, 28, true) +
+  int fail_count = 0;
+  // year case
+  // INFO 紀元前(year < 0)は未定義動作とする
+  fail_count +=
+      _exec_is_date(0, 1, 1, true) + _exec_is_date(9999, 12, 31, true) +
+      _exec_is_date(10000, 1, 1, true) + _exec_is_date(-1, 1, 1, true);
+
+  // month case
+  fail_count += _exec_is_date(2000, 0, 1, false) +
+                _exec_is_date(2000, 13, 1, false) +
+                _exec_is_date(2000, -1, 1, false);
+
+  // day case
+  fail_count +=
+      _exec_is_date(2000, 1, 0, false) + _exec_is_date(2000, 0, -1, false) +
+      _exec_is_date(2000, 4, 30, true) + _exec_is_date(2000, 4, 31, false) +
+      _exec_is_date(2000, 6, 30, true) + _exec_is_date(2000, 6, 31, false) +
+      _exec_is_date(2000, 9, 30, true) + _exec_is_date(2000, 9, 31, false) +
+      _exec_is_date(2000, 11, 30, true) + _exec_is_date(2000, 11, 31, false);
+
+  // leap case
+  fail_count +=
+      _exec_is_date(2000, 2, 28, true) + _exec_is_date(2000, 2, 29, true) +
+      _exec_is_date(2001, 2, 28, true) + _exec_is_date(2001, 2, 29, false) +
       _exec_is_date(2004, 2, 29, true) + _exec_is_date(2100, 2, 29, false) +
-      _exec_is_date(2000, 4, 31, false) + _exec_is_date(2000, 1, 0, false) +
-      _exec_is_date(2000, 12, 32, false) + _exec_is_date(2000, 13, 1, false) +
-      _exec_is_date(2001, 0, 1, false);
+      _exec_is_date(2400, 2, 29, true);
 
   if (!fail_count) {
     std::cout << "[OK]" << std::endl;
